c8aaeba49f40d647dd9da2e1b4cf2570aff01b39
jdurrant <jacobdurrant@gmail.com>
2023-04-15 21:41:39 -0400

    Minor update.

--- prepare_data/5.make_gninatypes.sh
+++ prepare_data/5.make_gninatypes.sh
@@ -3,7 +3,7 @@ module load parallel
 # find 1.pdbbind2020/ -name "*protein.pdb" | awk '{print "gninatyper " $1 " " $1 ".gninatypes"}' | sed "s/pdb.gninatypes/gninatypes/g" > t
 # find 1.pdbbind2020/ -name "*_ligand.sdf" | awk '{print "gninatyper " $1 " " $1 ".gninatypes"}' | sed "s/sdf.gninatypes/gninatypes/g" >> t
 
-find 1.pdbbind2020/ -name "*protein.pdb" | awk '{print "gninatyper " $1 " " $1 ".gninatypes"}' | sed "s/pdb.gninatypes/gninatypes/g" > t
-find 1.pdbbind2020/ -name "*_ligand.sdf" | awk '{print "gninatyper " $1 " " $1 ".gninatypes"}' | sed "s/sdf.gninatypes/gninatypes/g" >> t
+find 1.pdbbind2020/ -name "*protein_centered.pdb" | awk '{print "gninatyper " $1 " " $1 ".gninatypes"}' | sed "s/pdb.gninatypes/gninatypes/g" > t
+find 1.pdbbind2020/ -name "*_ligand_centered.sdf" | awk '{print "gninatyper " $1 " " $1 ".gninatypes"}' | sed "s/sdf.gninatypes/gninatypes/g" >> t
 
 . t | parallel
