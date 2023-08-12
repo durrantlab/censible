eca9404d0df5acde772916aa60ba38db9b6ff493
jdurrant <durrantj@pitt.edu>
2023-08-12 00:19:58 -0400

    Update to test_predict.sh

--- test_predict.sh
+++ test_predict.sh
@@ -2,6 +2,9 @@
 
 export smina_exec=`which smina`
 
+echo
+echo "smina path: " $smina_exec
+
 python predict.py --ligpath censible/data/test/1wdn_ligand.mol2 \
                   --recpath censible/data/test/1wdn_receptor.pdb \
                   --smina_exec_path $smina_exec \
