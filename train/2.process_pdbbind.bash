85191f697397f013579a5fad12d7b1d7aaf1ee95
jdurrant <durrantj@pitt.edu>
2023-09-14 21:30:45 -0400

    Renamed training files

---
+++ train/2.process_pdbbind.bash
@@ -0,0 +1,7 @@
+export OBABEL=/usr/bin/obabel
+
+# Prepare the receptors
+find pdbbind -name "*_protein.pdb" | awk '{print "echo " $1 "; grep -v HOH " $1 " > " $1 ".nowat.pdb; $OBABEL -ipdb " $1 ".nowat.pdb -opdb -d > " $1 ".nowat.noh.pdb; $OBABEL -ipdb " $1 ".nowat.noh.pdb -opdb -p 7 > " $1 ".nowat.ph7.pdb; rm " $1 ".nowat.pdb " $1 ".nowat.noh.pdb"}' | parallel
+
+# Prepare the ligands
+find pdbbind/ -name "*_ligand.mol2" | awk '{print "$OBABEL -omol2 " $1 " -omol2 -p 7 > " $1 ".ph7.mol2"}' | parallel
